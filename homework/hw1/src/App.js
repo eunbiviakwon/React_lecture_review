f15dec7741aad7aaa9f0d3f8f2feacf407a369d5
eunbiviakwon <eunbiviakwon@gmail.com>
2020-02-28 14:20:15 +0900

    hw1-1

--- homework/hw1/src/App.js
+++ homework/hw1/src/App.js
@@ -3,21 +3,42 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     username: '',
-    job: ''
+    password: '',
+    buttonact: 'disabled'
   };
-  handleChange = e => {
-    const {value, name} = e.target;
+  
+  handleChange = (e) => {
     this.setState({
-      [name]: value
+      [e.target.name]: e.target.value
     });
+    if (e.target.name === 'password') {
+      this.setState({
+        buttonact : e.target.value.length < 6 ? 'disabled' : false })
+    }
+
   };
+  
   render() {
+    const {username, password, buttonact} = this.state;
     return (
       <div>
-        
+        <input
+          name="username"
+          placeholder="유저명"
+          value={username}
+          onChange={e => this.handleChange(e)}
+          />
+        <input
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+          value={password}
+          onChange={this.handleChange}
+        />
+        <button type="submit" disabled={buttonact}>submit</button>
       </div>
     );
-  }
+}
 }
 
 export default App;
